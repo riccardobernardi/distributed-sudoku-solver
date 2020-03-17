0c1f86ce389315fd28e6ac62df3a4a2204d989da
riccardo bernardi <38067583+riccardobernardi@users.noreply.github.com>
2020-03-17 21:31:22 +0100

    cleaned up

---
+++ shutdown_cluster.py
@@ -0,0 +1,5 @@
+from rpicluster import *
+import pickle
+pickle.HIGHEST_PROTOCOL = 2
+
+shutdown()
---
+++ start_cluster.py
@@ -0,0 +1,6 @@
+from rpicluster import *
+import pickle
+pickle.HIGHEST_PROTOCOL = 2
+
+upload()
+start()
